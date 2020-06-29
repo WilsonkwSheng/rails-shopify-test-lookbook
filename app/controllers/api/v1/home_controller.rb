0510d632b5a6cd2005133dd27870e974f238ce58
wilson <wilsonkhoo93@gmail.com>
2020-06-29 09:27:43 +0800

    Add embed app to shopify

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::HomeController < ShopifyApp::AuthenticatedController
   def index
+    if !ShopifyAPI::ScriptTag.where(src: "#{request.base_url}/test.js").present?
+      ShopifyAPI::ScriptTag.create(event: "onload", src: "#{request.base_url}/test.js")
+    end
+    
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
 
     # TODO: when to save products from shopify?
