3669690e09c4e5e6163201bea3b9db09a0312cc0
wilson <wilsonkhoo93@gmail.com>
2020-06-29 10:05:53 +0800

    Log error json

--- public/test.js
+++ public/test.js
@@ -6,10 +6,12 @@ const handle = url.split('/').pop()
 fetch(`https://test-store-example1.myshopify.com/api/v1/front_end/index?shop=${shop}`, {
   method: "GET"
 })
-.then(res => res.json())
-.then(resp => {
-  let current_product = resp.filter(product => {
-    return product.shopify_handle === handle
-  })[0]
-  elem.innerHTML = current_product.lookbook_html
-})
+.then(res => res.text()) 
+.then(text => console.log(text))
+// .then(res => res.json())
+// .then(resp => {
+//   let current_product = resp.filter(product => {
+//     return product.shopify_handle === handle
+//   })[0]
+//   elem.innerHTML = current_product.lookbook_html
+// })
